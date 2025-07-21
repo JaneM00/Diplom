844edad452da49d18f20d9cfc08ece5d6e258c5d
JaneM00 <evg.mamikonyan@yandex.ru>
2025-07-21 20:21:34 +0300

    Update base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -23,3 +23,15 @@ class BasePage:
     def click(self, locator: tuple):
         self.find_element(locator).click()
         return self
+
+    @allure.step("Получить текст элемента {locator}")
+    def get_text(self, locator: tuple):
+        return self.find_element(locator).text
+
+    @allure.step("Перетащить элемент {source} на {target}")
+    def drag_and_drop(self, source: tuple, target: tuple):
+        ActionChains(self.driver).drag_and_drop(
+            self.find_element(source),
+            self.find_element(target)
+        ).perform()
+        return self
