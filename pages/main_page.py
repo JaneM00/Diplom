ead5919e5290e13355718b0e8190fc6f406b685c
JaneM00 <evg.mamikonyan@yandex.ru>
2025-07-21 20:22:49 +0300

    Update main_page.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,21 +1,26 @@
 # main_page.py
-from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from .base_page import BasePage
+from .urls import Urls
+import allure
 
 class MainPage(BasePage):
-    # ... существующие локаторы ...
+    CONSTRUCTOR_LINK = (By.XPATH, "//a[contains(@class, 'AppHeader_header__link') and contains(text(), 'Конструктор')]")
+    ORDER_FEED_LINK = (By.LINK_TEXT, "Лента заказов")
+    INGREDIENT_ITEM = (By.XPATH, "//div[contains(@class, 'Ingredient_ingredient')][1]")
+    INGREDIENT_MODAL = (By.XPATH, "//div[contains(@class, 'Modal_modal')]")
+    MODAL_CLOSE_BUTTON = (By.XPATH, "//button[contains(@class, 'Modal_close')]")
+    CONSTRUCTOR_AREA = (By.XPATH, "//div[contains(@class, 'BurgerConstructor_basket')]")
+    ORDER_BUTTON = (By.XPATH, "//button[contains(text(), 'Оформить заказ')]")
     INGREDIENT_COUNTER = (By.XPATH, "//div[contains(@class, 'Ingredient_counter')]")
-    INGREDIENT_DETAILS_NAME = (By.XPATH, "//div[contains(@class, 'Modal_modal')]//h3")
+    INGREDIENT_DETAILS = (By.XPATH, "//div[contains(@class, 'IngredientDetails_details')]")
+
+    @allure.step("Открыть главную страницу")
+    def open_main_page(self):
+        return self.open(Urls.BASE)
 
     @allure.step("Добавить ингредиент в конструктор")
-    def add_ingredient_to_constructor(self, ingredient_locator):
+    def add_ingredient_to_constructor(self, ingredient_type: str):
+        ingredient_locator = (By.XPATH, f"//div[contains(text(), '{ingredient_type}')]")
         self.drag_and_drop(ingredient_locator, self.CONSTRUCTOR_AREA)
         return self
-
-    @allure.step("Получить значение счетчика ингредиента")
-    def get_ingredient_counter_value(self, ingredient_locator):
-        counter = self.find_element(ingredient_locator.find_element(*self.INGREDIENT_COUNTER))
-        return int(counter.text) if counter.text else 0
-
-    @allure.step("Получить название ингредиента в модальном окне")
-    def get_ingredient_details_name(self):
-        return self.find_element(self.INGREDIENT_DETAILS_NAME).text
