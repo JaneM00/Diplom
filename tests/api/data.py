862c7469f06672c8f852b31b0095475af1f89bf7
JaneM00 <evg.mamikonyan@yandex.ru>
2025-07-22 23:32:38 +0300

    Update data.py

--- tests/api/data.py
+++ tests/api/data.py
@@ -5,17 +5,22 @@ fake = Faker()
 
 class TestData:
     @staticmethod
-    def random_user():
+    @allure.step("Генерация тестовых данных пользователя")
+    def generate_user():
         return {
             "email": fake.email(),
-            "password": fake.password(),
+            "password": fake.password(length=10),
             "name": fake.first_name()
         }
-    
-    @staticmethod
-    def valid_ingredients():
-        return ["61c0c5a71d1f82001bdaaa6d", "61c0c5a71d1f82001bdaaa6f"]
-    
+
     @staticmethod
     def invalid_ingredients():
-        return ["invalid_hash_123", "wrong_hash_456"]
+        return ["invalid_hash_1", "invalid_hash_2"]
+
+    @staticmethod
+    def incomplete_user_data():
+        return [
+            {"password": "password123", "name": "Test User"},  # Без email
+            {"email": "test@example.com", "name": "Test User"},  # Без пароля
+            {"email": "test@example.com", "password": "password123"}  # Без имени
+        ]
