d4c05b2aa30308afbda0d8c662ee721a438c7769
JaneM00 <evg.mamikonyan@yandex.ru>
2025-07-22 23:32:13 +0300

    Update helpers.py

--- tests/api/helpers.py
+++ tests/api/helpers.py
@@ -1,25 +1,38 @@
 # helpers.py
 import requests
+import allure
 from .urls import URLs
 
 class APIHelper:
-    @staticmethod
-    def register_user(user_data):
-        return requests.post(URLs.REGISTER, json=user_data)
-    
-    @staticmethod
-    def login_user(credentials):
-        return requests.post(URLs.LOGIN, json=credentials)
-    
-    @staticmethod
-    def delete_user(token):
-        return requests.delete(URLs.USER, headers={"Authorization": token})
-    
-    @staticmethod
-    def create_order(ingredients, token=None):
+    @allure.step("POST {url}")
+    def _post(self, url, json=None, headers=None):
+        return requests.post(url, json=json, headers=headers)
+
+    @allure.step("DELETE {url}")
+    def _delete(self, url, headers=None):
+        return requests.delete(url, headers=headers)
+
+    @allure.step("GET {url}")
+    def _get(self, url):
+        return requests.get(url)
+
+    @allure.step("Регистрация пользователя")
+    def register_user(self, user_data):
+        return self._post(URLs.REGISTER, json=user_data)
+
+    @allure.step("Авторизация пользователя")
+    def login_user(self, credentials):
+        return self._post(URLs.LOGIN, json=credentials)
+
+    @allure.step("Удаление пользователя")
+    def delete_user(self, token):
+        return self._delete(URLs.USER, headers={"Authorization": token})
+
+    @allure.step("Создание заказа")
+    def create_order(self, ingredients, token=None):
         headers = {"Authorization": token} if token else {}
-        return requests.post(URLs.ORDERS, json={"ingredients": ingredients}, headers=headers)
-    
-    @staticmethod
-    def get_ingredients():
-        return requests.get(URLs.INGREDIENTS)
+        return self._post(URLs.ORDERS, json={"ingredients": ingredients}, headers=headers)
+
+    @allure.step("Получение списка ингредиентов")
+    def get_ingredients(self):
+        return self._get(URLs.INGREDIENTS)
