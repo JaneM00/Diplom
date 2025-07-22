dab78f44d9740ec7a449affbc3caa508c7b7e81b
JaneM00 <evg.mamikonyan@yandex.ru>
2025-07-22 23:45:35 +0300

    Update test_main_page.py

--- tests/ui/test_main_page.py
+++ tests/ui/test_main_page.py
@@ -1,18 +1,32 @@
 # test_main_page.py
 import allure
+import allure
 
 @allure.feature("Главная страница")
 class TestMainPage:
     @allure.story("Навигация")
     @allure.title("Переход в конструктор")
-    def test_constructor_navigation(self, main_page):
-        main_page.open_main_page()
-        main_page.click(main_page.CONSTRUCTOR_LINK)
-        assert "current" in main_page.get_element_attribute(main_page.CONSTRUCTOR_LINK, "class")
+    def test_navigate_to_constructor(self, authorized_user):
+        with allure.step("Кликнуть на ссылку конструктора"):
+            authorized_user.navigate_to_constructor()
+        
+        with allure.step("Проверить активное состояние"):
+            assert authorized_user.is_constructor_active()
 
-    @allure.story("Модальное окно")
-    @allure.title("Просмотр деталей ингредиента")
-    def test_ingredient_details(self, main_page):
-        main_page.open_main_page()
-        main_page.click(main_page.INGREDIENT_ITEM)
-        assert main_page.is_element_visible(main_page.INGREDIENT_DETAILS)
+    @allure.story("Навигация")
+    @allure.title("Переход в ленту заказов")
+    def test_navigate_to_order_feed(self, authorized_user):
+        with allure.step("Кликнуть на ссылку ленты заказов"):
+            authorized_user.navigate_to_order_feed()
+        
+        with allure.step("Проверить отображение ленты"):
+            assert authorized_user.is_order_feed_visible()
+
+    @allure.story("Интерактивные элементы")
+    @allure.title("Отображение деталей ингредиента")
+    def test_ingredient_details_display(self, authorized_user):
+        with allure.step("Кликнуть на ингредиент"):
+            authorized_user.open_ingredient_details("Булка")
+        
+        with allure.step("Проверить отображение деталей"):
+            assert authorized_user.are_ingredient_details_visible()
